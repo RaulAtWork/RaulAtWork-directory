802fe06d4af7ec4a12174c1696124d8ec209d5c8
Raul <raulescabia.work@gmail.com>
2024-06-19 19:21:22 +0200

    simple table example

--- src/components/ProjectTable.jsx
+++ src/components/ProjectTable.jsx
@@ -1,9 +1,73 @@
-import React from "react";
+import React, { useMemo } from "react";
+import { useTable, useSortBy } from "react-table";
 
-function ProjectTable(){
-    return(<>
-        <h2>This is the project table</h2>
-    </>)
+// Define your table component
+function ProjectsTable({ data }) {
+  const columns = useMemo(
+    () => [
+      {
+        Header: "Project Name",
+        accessor: "name",
+      },
+      {
+        Header: "Status",
+        accessor: "status",
+      },
+    ],
+    []
+  );
+
+  // Use the useTable hook to create your table instance
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable(
+      {
+        columns,
+        data,
+      },
+      useSortBy
+    );
+
+  // Render your table
+
+  return (
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map((headerGroup) => (
+          <tr key={headerGroup.id} {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map(
+              (column) =>
+                (
+                  <th key={column.id} {...column.getHeaderProps(column.getSortByToggleProps())}>
+                    {column.render("Header")}
+<span>
+                 {column.isSorted ? (column.isSortedDesc ? ' 🔽' : ' 🔼') : ''}
+              </span>
+                  </th>
+                )
+            )}
+          </tr>
+        ))}
+      </thead>
+
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row);
+
+          return (
+            <tr key={row.id} {...row.getRowProps()}>
+              {row.cells.map((cell) => {
+                return (
+                  <td key={cell.column.id} {...cell.getCellProps()}>
+                    {cell.render("Cell")}
+                  </td>
+                );
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 }
 
-export default ProjectTable
+export default ProjectsTable;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useMemo } from "react";
 import ProjectTable from "../components/ProjectTable";
 
 function Home(){
+
+    const projectData = useMemo(() => [{
+        name: "Test",
+        status: "in-progress"
+    },
+    {
+        name: "Test2",
+        status: "in-progress"
+    },
+    {
+        name: "Test3",
+        status: "in-progress"
+    }], [])
+
     return (<>
         <h1>Welcome home captain.</h1>
-        <ProjectTable/>
+        <ProjectTable data={projectData}/>
     </>)
 }
 
